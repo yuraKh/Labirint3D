7ebeada308ab2d59b308645c664ad4caa3f5ee38
yuraKh <yurakhodanitcky@gmail.com>
2015-04-20 00:13:11 +0300

    0008

--- Assets/AnemyControl.cs
+++ Assets/AnemyControl.cs
@@ -3,41 +3,98 @@ using System.Collections;
 
 public class AnemyControl : MonoBehaviour
 {
-	public GameObject zombak;
-	GameObject ArmyOfDead;
-	float[] distan;
-	GameObject[] dist;
 
-	public GameObject FindClosestGem () // Пошук найближчого
-	{ 
-		GameObject[] gos;
-		gos = GameObject.FindGameObjectsWithTag ("cube");
-		GameObject closest = null;
-		float distance = Mathf.Infinity;
-		Vector3 position = transform.position;
-		foreach (GameObject go in gos) {
-			Vector3 diff = go.transform.position - position;
-			float curDistance = diff.sqrMagnitude;
-			if (curDistance < distance) {
-				closest = go;
-				distance = curDistance;
-			}
-		}
-		return closest;
-	}
+    bool GamIsTacken = false;
+    GameObject[] Zombie;
+    GameObject[] Zomb;
 
 
-	void Start ()
-	{
+    GameObject FindClosestGem() // Пошук найближчого
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("cube");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
 
-	}
+    void Start()
+    {
 
-	void Update ()
-	{
-		if (Input.GetMouseButtonDown (0)) {
-			print (FindClosestGem ().name);
-			ArmyOfDead = (GameObject)Instantiate (zombak, transform.position, Quaternion.identity);
-			ArmyOfDead.GetComponent<NavMeshAgent> ().SetDestination (FindClosestGem ().transform.position);
-		}
-	}
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "cube")
+        {
+            print(FindClosestGem().name);
+            FindClosestGem().transform.SetParent(transform);
+            FindClosestGem().tag = "cub";
+            transform.tag = "Zomb";
+        }
+
+        if (other.tag == "cub")
+        {
+            GamIsTacken = true;
+            other.transform.GetComponent<BoxCollider>().enabled = false;
+        }
+
+        if ((other.name == "Sphere") && (GamIsTacken))
+        {
+            print("no");
+            Destroy(gameObject);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        Zombie = GameObject.FindGameObjectsWithTag("Zombie");
+        for (int i = 0; i < Zombie.Length; i++)
+        {
+            if (FindClosestGem() != null)
+            {
+                Zombie[i].GetComponent<NavMeshAgent>().SetDestination(FindClosestGem().transform.position);
+            }
+            else Zombie[i].GetComponent<NavMeshAgent>().SetDestination(GameObject.Find("Sphere").transform.position);
+        }
+        Zomb = GameObject.FindGameObjectsWithTag("Zomb");
+        foreach (GameObject zzomb in Zomb)
+        {
+            if (zzomb != null)
+            {
+                zzomb.GetComponent<NavMeshAgent>().SetDestination(GameObject.Find("Sphere").transform.position);
+            }
+            else break;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (GamIsTacken)
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    if (transform.GetChild(i).tag == "cub")
+                    {
+                        transform.GetChild(i).position = new Vector3(transform.GetChild(i).position.x, 0.5f, transform.GetChild(i).position.z);
+                        transform.GetChild(i).tag = "cube";
+                        transform.GetChild(i).GetComponent<BoxCollider>().enabled = true;
+                        transform.GetChild(i).transform.parent = null;
+                    }
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/ZombieReturn.cs
+++ Assets/ZombieReturn.cs
@@ -3,42 +3,14 @@ using System.Collections;
 
 public class ZombieReturn : MonoBehaviour
 {
-	bool GamIsTacken = false;
+    public GameObject zombak;
+    GameObject ArmyOfDead;
 
-
-	void Start ()
-	{
-
-	}
-	void OnTriggerEnter (Collider other)
-	{
-		if (other.tag == "cube") {
-			print ("yes");
-			//Gem.transform.SetParent (transform);
-			//GetComponent<AnemyControl> ().FindClosestGem ().transform.SetParent (transform);
-			other.gameObject.transform.SetParent (transform);
-			GetComponent<NavMeshAgent> ().SetDestination (GameObject.Find ("Sphere").transform.position);
-			GamIsTacken = true;
-		}
-		if ((other.name == "Sphere") && (GamIsTacken)) {
-			print ("no");
-			Destroy (gameObject);
-		}
-	}
-
-
-	void Update ()
-	{
-		if (Input.GetMouseButtonDown (1)) {
-			if (GamIsTacken) {
-				for (int i = 0; i < transform.childCount; i++) {
-					if (transform.GetChild (i).tag == "cube") {
-						transform.GetChild (i).position = new Vector3 (transform.GetChild (i).position.x, 0.5f, transform.GetChild (i).position.z);
-						transform.GetChild (i).transform.parent = null;
-					}
-				}
-			} 
-			Destroy (gameObject);
-		}
-	}
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ArmyOfDead = (GameObject)Instantiate(zombak, GameObject.Find("Sphere").transform.position, Quaternion.identity);
+        }
+    }
 }
